e5878860c6fd0945fd7782d23eb2de61e67e63ad
DmytroMitin <dmitin3@gmail.com>
2023-03-28 00:05:30 +0300

    Use c.eval

--- macros/src/main/scala/com/dv/phoenix/brandsafety/utils/RunningStrategiesMacros.scala
+++ macros/src/main/scala/com/dv/phoenix/brandsafety/utils/RunningStrategiesMacros.scala
@@ -1,7 +1,7 @@
 package com.dv.phoenix.brandsafety.utils
 
-import com.dv.phoenix.brandsafety.models.{RunningStrategies, RunningStrategy}
-import com.dv.phoenix.brandsafety.models.RunningStrategy.RunningStrategy
+import com.dv.phoenix.brandsafety.models.RunningStrategies
+import com.dv.phoenix.brandsafety.models.RunningStrategy.{MonitoringAndBlockingInherent, MonitoringAndBlockingRecalculate, RunningStrategy}
 
 import scala.reflect.macros.blackbox
 
@@ -9,17 +9,14 @@ object RunningStrategiesMacros {
   def applyImp(c: blackbox.Context)(strategies: c.Expr[RunningStrategy]*): c.Expr[RunningStrategies] = {
     import c.universe._
 
-    val strategySet = strategies.map(_.tree).toList
-
-    val cond = List("MonitoringAndBlockingInherent", "MonitoringAndBlockingRecalculate")
-      .map(name => typeOf[RunningStrategy.type].decl(TermName(name)))
-      .forall(strategySet.map(_.symbol).contains)
-
+    val cond = Seq(MonitoringAndBlockingInherent, MonitoringAndBlockingRecalculate).forall(
+      strategies.map(expr => c.eval(c.Expr[RunningStrategy](c.untypecheck(expr.tree)))).contains
+    )
     if (cond) {
       c.abort(c.enclosingPosition, "MonitoringAndBlockingInherent and MonitoringAndBlockingRecalculate cannot be used together")
     }
 
-    val runningStrategies = q"RunningStrategies(Set(..$strategySet))"
+    val runningStrategies = q"RunningStrategies(Set(..$strategies))"
     c.Expr[RunningStrategies](runningStrategies)
   }
 }
